53e68bbc42f23f5782a0743bd3cf4374ba680be9
oscar2682 <oscar.cas.art@gmail.com>
2020-04-18 15:00:31 -0500

    Select button ready to parse filename

--- main-gui_MT_preproc.py
+++ main-gui_MT_preproc.py
@@ -4,17 +4,13 @@
 GUI to preprocess raw data directly from TEM equipment.
 
 It shows a time series and allows you to select points, either one by one or by a time window, and removes the selected points
+
+Developed by: Oscar Castro-Artola. April, 2020. E-Mail:oscar.castro@unicach.mx"
 """
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QDialog,QApplication
 from PyQt5.QtWidgets import QPushButton,QVBoxLayout
 from PyQt5.QtWidgets import QGroupBox,QFileDialog
-
-
-
-
-
-
 
 class MainWindow(QDialog):
     # MAIN FUNCTION
@@ -24,21 +20,22 @@ class MainWindow(QDialog):
         self.setWindowTitle(self.tr("Pre-Process raw TEM data"))
         self.group_1 = QGroupBox("-- Selecting data --", self)
         self.vbox_1 = QVBoxLayout()
-        self.bot_get_data_file = QPushButton("Choose your fucking files")
+        self.bot_get_data_file = QPushButton("Select files")
         self.bot_get_data_file.setDisabled(False)
         self.vbox_1.addWidget(self.bot_get_data_file)
-        self.bot_get_data_file.clicked.connect(self.get_data)
+        self.bot_get_data_file.clicked.connect(self.select_file)
         self.group_1.setLayout(self.vbox_1)
 
-    def get_data(self):
+    def select_file(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Select files to plot"\
+        fileName, nada = QFileDialog.getOpenFileName(self,"Select files to plot"\
                 , ""\
-                ,"All Files (*);;Python Files (*.py)"\
+                ," Universal Sounding Format Files (*.usf)"\
                 , options=options)
         if fileName:
             print(fileName)
+            open(fileName)
 
 
 if __name__ == "__main__":
